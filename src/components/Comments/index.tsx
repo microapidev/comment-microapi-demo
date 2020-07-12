8d7fc1a7cf46e417e0eb790599a842fd6598982a
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-07-12 20:00:17 +0200

    feat: add comments page tab view container

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,44 +1,99 @@
-import React from "react";
-import {
-  Switch,
-  Route,
-  Link,
-  useParams,
-  useRouteMatch
-} from "react-router-dom";
+import React, { FunctionComponent } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
-function Comments() {
-  let { path, url } = useRouteMatch();
+type TabPanelProps = {
+  children: string;
+  index: number;
+  value: number;
+  other?: any;
+};
 
+const TabPanel: FunctionComponent<TabPanelProps> = ({
+  children,
+  index,
+  value,
+  other,
+}) => {
   return (
-    <div style={{ margin: "1rem" }}>
-      <h2>Choose comments demo:</h2>
-      <nav style={{ display: "flex", flexDirection: "column" }}>
-        <Link to={`${url}/create-comment`}>Create</Link>
-        <Link to={`${url}/get-comment`}>Get</Link>
-        <Link to={`${url}/update-comment`}>Update</Link>
-        <Link to={`${url}/delete-comment`}>Delete</Link>
-        <Link to={`${url}/vote-comment`}>Vote</Link>
-        <Link to={`${url}/flag-comment`}>Flag</Link>
-      </nav>
-
-      <Switch>
-        <Route path={`${path}/:demo`}>
-          <CommentsDemo />
-        </Route>
-      </Switch>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
     </div>
   );
+};
+
+function a11yProps(index: number) {
+  return {
+    id: `comment-tab-${index}`,
+    "aria-controls": `comment-tabpanel-${index}`,
+  };
 }
 
-function CommentsDemo() {
-  let { demo } = useParams();
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+const Comments: FunctionComponent = () => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: any, newValue: number) => {
+    setValue(newValue);
+  };
 
   return (
-    <div>
-      <h4>{demo}</h4>
+    <div className={classes.root}>
+      <AppBar position="static" elevation={0}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="The tabs for the different comment functionalities."
+        >
+          <Tab label="Comment Create" {...a11yProps(0)} />
+          <Tab label="Comment Get" {...a11yProps(1)} />
+          <Tab label="Comment Update" {...a11yProps(2)} />
+          <Tab label="Comment Delete" {...a11yProps(3)} />
+          <Tab label="Comment Vote" {...a11yProps(4)} />
+          <Tab label="Comment Flag" {...a11yProps(5)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Comment Create Tab Panel
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Comment Get Tab Panel
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Comment Update Tab Panel
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Comment Delete Tab Panel
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Comment Vote Tab Panel
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        Comment Flag Tab Panel
+      </TabPanel>
     </div>
   );
-}
+};
 
 export default Comments;
